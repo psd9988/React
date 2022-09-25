1e9c4d72137d7993b228c1b82c706bc7d7fc7a67
psd9988 <psd9988@gmail.com>
2022-09-25 14:03:41 +0530

    2nd Commit

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,18 +1,21 @@
 import React, { useState } from 'react'
+// import { Display } from './Display';
 import './Todo.css'
 
 export function Todo() {
 
     const [todo, setTodo] = useState("");
+    const [saveToDoList, setTodoList] = useState([]);
 
     const changedInput = (e) => {
         setTodo(e.target.value);
-        console.log(todo)
     }
 
     const formSubmitFunc = (e) => {
         e.preventDefault();
- 
+        const newToDo = todo;
+        setTodoList([...saveToDoList, newToDo])
+        setTodo('')
     }
 
     return (
@@ -20,9 +23,19 @@ export function Todo() {
             <h1 className="mainHeading">My To Do App</h1>
             <form onSubmit={formSubmitFunc}>
 
-                <input type="text" name="addToDo" value={todo} onChange= {changedInput} />
+                <input type="text" name="addToDo" value={todo} onChange={changedInput} />
                 <button>Add todo</button>
 
+                {
+                    saveToDoList.map((eachToDo, index) =>
+                    (
+                        <div className='displayContainer' key={`todo ${index}`}>
+                            <h3>{eachToDo}</h3>
+                            <button>Delete</button>
+                            <button>Edit</button>
+                        </div>
+                    ))
+                }
             </form>
         </div>
     )
